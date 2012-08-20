cc2922f13629adfc13f054fdbab29b4b940a6524
Kristian Øllegaard <kristian@kristian.io>
2012-08-20 16:52:36 +0200

    Le settings

---
+++ pelican.conf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+AUTHOR = u"Kristian Øllegaard"
+SITENAME = "Kristian's blog"
+SITEURL = "http://blog.kristian.io"
+TIMEZONE = "CET"
+
+# Theme specific
+
+TWITTER_USERNAME = "oellegaard"
+GITHUB_URL = "http://www.github.com/KristianOellegaard/blog.kristian.io/"
+
+# Looks
+
+THEME = 'notmyidea'
